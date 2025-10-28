154b52decba9cf75eb0a50e75da82f7855865d9b
misaka10987 <misaka10987@outlook.com>
2025-10-28 12:05:53 +0800

    refactor `Point` with OOP

--- Archimedes/Basic.lean
+++ Archimedes/Basic.lean
@@ -10,40 +10,68 @@ For declaring a point, simply use `p : Point`.
 
 This is an alias for Mathlib's `EuclideanSpace ℝ (Fin 3)`,
 and thus has exactly the same behaviour.
-
-# Component Convention
-
-Traditionally,
-$x$, $y$ and $z$ is usually used to name the components of a 3-dimensional vector.
-Since `EuclideanSpace ℝ (Fin 3)` is in fact a function from `Fin 3` to `ℝ`,
-and components are accessed with function call,
-for consistency, it is specified here that for `p : Point`:
-- `p 0` is the $x$ component;
-- `p 1` is the $y$ component;
-- `p 2` is the $z$ component.
 -/
 abbrev Point := EuclideanSpace ℝ (Fin 3)
 
+namespace Point
 
-infix:69 "⬝" => inner ℝ
+/--
+The x-component of the vector, defined as `self 0` .
+-/
+@[simp]
+abbrev x (self : Point) : ℝ := self 0
+
+/--
+The y-component of the vector, defined as `self 1` .
+-/
+@[simp]
+abbrev y (self : Point) : ℝ := self 1
+
+/--
+The z-component of the vector, defined as `self 2` .
+-/
+@[simp]
+abbrev z (self : Point) : ℝ := self 2
+
+/--
+The dot product.
+-/
+@[simp]
+noncomputable abbrev dot (self : Point) (v : Point) : ℝ :=
+  Inner.inner ℝ self v
+
+/--
+The dot product.
+-/
+infix:69 "⬝" => dot
+
+/--
+The cross product.
+-/
+@[simp]
+abbrev cross (self : Point) (v : Point) : Point :=
+  crossProduct self v
+
+/--
+The cross product.
+-/
+infix:69 "⨯" => cross
+
+end Point
 
 /--
 Naive definition of dot product of 3-dimensional vectors,
 i.e. sum of product of corresponding components.
 -/
-theorem dot (v w : Point) : v ⬝ w = v 0 • w 0 + v 1 • w 1 + v 2 • w 2 := by
+theorem inner_product (v w : Point) : v ⬝ w = v.x • w.x + v.y • w.y + v.z • w.z := by
   simp [Inner.inner, Fin.sum_univ_succ]
   ring
-
-
-infix:69 "⨯" => crossProduct
-
 
 /--
 Naive definition of cross product of 3-dimensional vectors.
 -/
-theorem cross (v w : Point)
-: v ⨯ w = ![v 1 • w 2 - v 2 • w 1, v 2 • w 0 - v 0 • w 2, v 0 • w 1 - v 1 • w 0 ]
+theorem vector_product (v w : Point)
+: v ⨯ w = ![ v.y • w.z - v.z • w.y, v.z • w.x - v.x • w.z, v.x • w.y - v.y • w.x ]
 := by
   simp [crossProduct]
 
@@ -52,33 +80,33 @@ theorem cross (v w : Point)
 Naive definition of normal of 3-dimensional vector,
 i.e. square root of sum of components squared.
 -/
-theorem norm (v : Point) : ‖v‖ = √ (v 0 ^ 2 + v 1 ^ 2 + v 2 ^ 2) := by
+theorem norm (v : Point) : ‖v‖ = √ (v.x ^ 2 + v.y ^ 2 + v.z ^ 2) := by
   simp [Norm.norm, Fin.sum_univ_succ, Real.sqrt_eq_rpow]
   ring_nf
 
 /--
 Square of normal of a vector, or sum of square of components, is non-negative.
 -/
-theorem sq_norm_nn (v : Point) : 0 ≤ v 0 ^ 2 + v 1 ^ 2 + v 2 ^ 2 := by
+theorem sq_norm_nn (v : Point) : 0 ≤ v.x ^ 2 + v.y ^ 2 + v.z ^ 2 := by
   repeat first | apply add_nonneg | apply sq_nonneg
 
 /--
 Square of normal of a vector equals to the sum of square of components.
 -/
-theorem sq_norm (v : Point) : ‖v‖ ^ 2 = v 0 ^ 2 + v 1 ^ 2 + v 2 ^ 2 := by
+theorem sq_norm (v : Point) : ‖v‖ ^ 2 = v.x ^ 2 + v.y ^ 2 + v.z ^ 2 := by
   simp [norm, Real.sq_sqrt (sq_norm_nn v)]
 
 /--
 A vector dot products itself equals square of its normal.
 -/
 theorem sq_norm_eq_dot_self (v : Point) : ‖v‖ ^ 2 = v ⬝ v := by
-  simp [dot, norm, pow_two]
+  simp [inner_product, norm, pow_two]
   rw [←Real.sqrt_mul, Real.sqrt_mul_self]
   repeat first | apply add_nonneg | apply mul_self_nonneg
 
 
 /--
-The origin point, or $(0,0,0)$.
+The origin point, or $(0, 0, 0)$.
 -/
 abbrev o : Point := 0
 
@@ -127,7 +155,7 @@ lemma norm_unit_is_one (v : Point) : ‖unit v‖ = 1 := by
 A vector dot products its unit vector equals its normal.
 -/
 theorem dot_unit_eq_norm (v : Point) : v ⬝ unit v = ‖v‖ := by
-  simp [unit, dot]
+  simp [unit, inner_product]
   by_cases h : v = 0
   · simp [h]
   · simp [h]
